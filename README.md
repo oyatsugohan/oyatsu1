9ad78232a083678ea26393127e37a1714a8691ce
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-22 07:50:19 +0000

    iuhfu

--- README.md
+++
@@ -1,5 +0,0 @@
-import streamlit
-
-st.title('あいうえお)
-
-st.write('aaaa')
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ st.title('自己紹介')
 
 st.write('まずは名前を教えて')
 
-user_name=st.text_imput('名前を入力してください')
+user_name=st.text_input('名前を入力してください')
 
 st.header('あなたの名前は'+str(user_name)+'です')
+
+h=st.text_input('身長を入力してください(m)')
+W=st.text_input('体重を入力してください(kg)')
+
+bmi=
