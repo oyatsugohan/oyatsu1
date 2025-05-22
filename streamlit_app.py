4361701f7123bcd64e69e71333e218a659bfe15b
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-22 08:11:24 +0000

    中二病

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.title('自己紹介')
 
-st.write('まずは名前を教えて')
+st.write('汝の名を刻め')
 
 user_name=st.text_input('名前を入力してください')
 
-st.header('あなたの名前は'+str(user_name)+'です')
+st.header('汝の名は'+str(user_name)+'だ')
 
 h=st.number_input('身長を入力してください(m)',value=1.70)
 w=st.number_input('体重を入力してください(kg)',value=70)
