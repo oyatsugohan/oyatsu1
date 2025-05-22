6fa9151051654f63f00bfa50dba4e0e217ec7a1b
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-22 07:55:24 +0000

    iawhfl

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,11 @@ user_name=st.text_input('名前を入力してください')
 
 st.header('あなたの名前は'+str(user_name)+'です')
 
-h=st.text_input('身長を入力してください(m)')
-W=st.text_input('体重を入力してください(kg)')
+h=st.text_input('身長を入力してください(m)',value=1.70)
+W=st.text_input('体重を入力してください(kg)',value=70)
 
-bmi=
+bmi=w/(h**2)
+
+dbmi=round(bmi,2)
+
+st.header('あなたのBMIは'+str(dbmi)+'です')
