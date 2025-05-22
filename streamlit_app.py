f23e88ab1de73af4a34cb267464b398d4aec4fd6
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-22 07:57:44 +0000

    修正完了

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ user_name=st.text_input('名前を入力してください')
 
 st.header('あなたの名前は'+str(user_name)+'です')
 
-h=st.text_input('身長を入力してください(m)',value=1.70)
-W=st.text_input('体重を入力してください(kg)',value=70)
+h=st.number_input('身長を入力してください(m)',value=1.70)
+w=st.number_input('体重を入力してください(kg)',value=70)
 
 bmi=w/(h**2)
 
