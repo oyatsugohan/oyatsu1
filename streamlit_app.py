5603329873fa4dac21ba8bf021ebce955742ef3e
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-29 08:12:07 +0000

    まんまとかかったまみむめも

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ if difficulty == '１':
     st.write ('難易度：高校１年生')
     st.write (' ')
     question_1 = st.text_input('問題１　「重要な、大切な」') 
-    if question_1 == important:
+    if question_1 == 'important':
         st.write('正解！')
     else:
         st.write('不正解　解答：important')
