f8d8df06a007d30a3c092d7dbc81f15c2d7441b9
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-29 07:55:43 +0000

    笹の葉獅子の毛さしすせそ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title('英単語学習アプリ')
 
 st.write('難易度を選択してください')
 difficulty = st.text_input ('簡単ならeasy,難しめならdifficult')
-if difficulty == easy:
+if difficulty == 'easy':
     st.write ('難易度：easy')
 else:
     st.write('難易度：difficult')
