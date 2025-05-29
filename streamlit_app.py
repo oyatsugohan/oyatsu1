86a00f83a6184ac44b943f197bda269b43903e1e
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-29 07:51:07 +0000

    アメンボ赤いなあいうえお

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title('英単語学習アプリ')
+
+st.write('難易度を選択してください')
+difficulty = st.text_input ('簡単ならeasy,難しめならdifficult')
+if difficulty == easy:
+    st.write ('難易度：easy')
+elif:
+    st.write('難易度：difficult')
