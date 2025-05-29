9e2479717af117a32193c37881f0362df82a0469
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-29 08:36:48 +0000

    おあｓかｋそどｗどｊふぉｗｊふぃｊｒｇｊれｇｈｗｇｊｇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if difficulty == '１':
     else:
         question_1_answer = st.write('不正解　解答：important')
         
-    question_2 = st.write ('問題２　「」')
+    question_2 = st.write ('問題２　「しかし、けれども」')
     if question_2 == (''):
         st.write('入力してください')
     elif question_2 == 'however':
