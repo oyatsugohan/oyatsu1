a7a02665aff58139ac6b26a459e4aec87b8ab69a
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-29 07:58:40 +0000

    七人クソガキなにぬねの

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write('難易度を選択してください')
 difficulty = st.text_input ('簡単ならeasy,難しめならdifficult')
 if difficulty == 'easy':
     st.write ('難易度：easy')
-elif difficulty == 'difficult'
+elif difficulty == 'difficult':
     st.write('難易度：difficult')
 else:
     st.write('難易度を設定してください')
