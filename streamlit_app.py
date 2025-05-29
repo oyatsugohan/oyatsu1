62ca6b432c0160f185c569994785bc73184591ef
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-05-29 08:40:41 +0000

    ;oijge

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('英単語学習アプリ')
 st.write('日本語訳を英単語に直しなさい')
 st.write('難易度を選択してください')
 difficulty = st.text_input ('高校１年生レベルなら１,高校２～３年生レベルなら２')
-if difficulty == '１':
+if difficulty == '１' or '1':
     st.write ('難易度：高校１年生')
     st.write (' ')
     question_1 = st.text_input('問題１　「重要な、大切な」') 
@@ -24,7 +24,7 @@ if difficulty == '１':
         st.write('正解')
     else:
         st.write('不正解　解答：however')
-elif difficulty == '２':
+elif difficulty == '２' or '2':
     st.write('難易度：高校２～３年生')
 else:
     st.write('難易度を設定してください')
