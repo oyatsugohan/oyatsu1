7e512849615e788839055e7f6fadbb0723b93f5a
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-06-05 08:44:22 +0000

    方向性決定

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,12 @@
 import streamlit as st
+import random
 
-st.title('英単語学習アプリ')
+st.title('未定：')
 
-st.write('日本語訳を英単語に直しなさい')
-st.write('難易度を選択してください')
-difficulty = st.text_input ('高校１年生レベルなら１,高校２～３年生レベルなら２')
-if difficulty == '':
-    print(' ')
-elif difficulty == '１' or '1':
-    st.write ('難易度：高校１年生')
-    question_1 = st.text_input('問題１　「重要な、大切な」') 
-    if question_1 == '':
-        st.write('入力してください')
-    elif question_1 == "important":
-        st.write('正解！')
-        
-    else:
-        question_1_answer = st.write('不正解　解答：important')
-        
-    question_2 = st.text_input ('問題２　「しかし、けれども」')
-    if question_2 == '':
-        st.write('入力してください')
-    elif question_2 == 'however':
-        st.write('正解')
-    else:
-        st.write('不正解　解答：however')
-elif difficulty == '２' or '2':
-    st.write('難易度：高校２～３年生')
-else:
-    st.write('難易度を設定してください')
+player_name='player'
+player_name=st.text_input('playerの名前を決定してください')
+
+destination=st.text_input('向かう場所を設定してください')
+
+if destination==('初めの森'):
+    
