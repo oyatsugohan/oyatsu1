a044cd53c73b56e7d6b92301aedefc356f0aa37f
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-06-10 08:48:03 +0000

    efrenlrenfenfe

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ def main():
             st.write('名称未定(漢検二級)')
             st.write('名称未定(漢検準一級)')
             
-        expect Exception as e:
+        except Exception as e:
             st.error(f'なんかちがーう:{e}')
