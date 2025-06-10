7af32a1d3af1fcced10c7fba7751c7fda5ebfb44
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-06-10 08:36:55 +0000

    jiewahvugdsgljreaigre

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
 import streamlit as st
-
+import pandas as pd
 import random
 
-st.title('未定：')
+def main():
+    st.title('未定：')
+    uploaded_file = st.file_uploader("Excelファイルをアップロードしてください", type=["xlsx", "xls"])
+    if uploaded_file:
+        try:
+            player_name='player'
+            player_name=st.text_input('あなたの名前を決定してください')
+            st.write('あなたの名前は'+player_name+'です！')
 
-player_name='player'
-player_name=st.text_input('playerの名前を決定してください')
-
-destination=st.text_input('向かう場所を設定してください(始まりの森(漢検三級)なら「始まりの森」)')
-st.write('始まりの森(漢検三級)')
-st.write('名称未定(漢検準二級)')
-st.write('名称未定(漢検二級)')
-st.write('名称未定(漢検準一級)')
-if destination==('始まりの森'):
-    
+            destination=st.text_input('向かう場所を設定してください(始まりの森(漢検三級)なら「始まりの森」)')
+            st.write('始まりの森(漢検三級)')
+            st.write('名称未定(漢検準二級)')
+            st.write('名称未定(漢検二級)')
+            st.write('名称未定(漢検準一級)')
+            if destination==('始まりの森'):
