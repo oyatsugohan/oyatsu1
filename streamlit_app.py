d620b5e8a19fca2a9c73371a46c5a6db4142aff3
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-06-10 08:06:20 +0000

    難易度設定完了

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,10 @@ st.title('未定：')
 player_name='player'
 player_name=st.text_input('playerの名前を決定してください')
 
-destination=st.text_input('向かう場所を設定してください')
+destination=st.text_input('向かう場所を設定してください(始まりの森(漢検三級)なら「始まりの森」)')
 st.write('始まりの森(漢検三級)')
-if destination==(''):
+st.write('名称未定(漢検準二級)')
+st.write('名称未定(漢検二級)')
+st.write('名称未定(漢検準一級)')
+if destination==('始まりの森'):
     
