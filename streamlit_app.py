24b42f6048fc5dfb8d6a3c1372650bec8cf7d818
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-07-03 08:12:20 +0000

    ルルブ

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,12 @@ player_name='player'
 player_name=st.text_input('あなたの名前を決定してください')
 st.write('あなたの名前は'+player_name+'です！')
 
-destination=st.text_input('向かう場所を設定してください(始まりの森(漢検三級)なら「始まりの森」)')
-st.write('始まりの森(漢検三級)')
-st.write('名称未定(漢検準二級)')
-st.write('名称未定(漢検二級)')
-st.write('名称未定(漢検準一級)')
+if st.write('あなたの名前は'+player_name+'です！'):
+    destination=st.text_input('向かう場所を設定してください(始まりの森(漢検三級)なら「始まりの森」)')
+    st.write('始まりの森(漢検三級)')
+    st.write('名称未定(漢検準二級)')
+    st.write('名称未定(漢検二級)')
+    st.write('名称未定(漢検準一級)')
 
 if destination=='始まりの森':
     selected_row = df.iloc[fixed_row_index]
