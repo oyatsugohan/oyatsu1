fe62a5d0c1421988911f63c6402f74f5ce74fcf1
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-07-08 08:39:45 +0000

    fire◎flower

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,8 @@ player_name='player'
 player_name=st.text_input('あなたの名前を決定してください')
 st.write('あなたの名前は'+player_name+'です！')
 
+col_a,col_b,col_c = st.columns(3)
 
-destination=st.text_input('向かう場所を設定してください(始まりの森(漢検三級)なら「始まりの森」)')
-st.write('始まりの森(漢検三級)')
-st.write('名称未定(漢検準二級)')
-st.write('名称未定(漢検二級)')
-st.write('名称未定(漢検準一級)')
-
-enemy_1=('コボルト','ゴブリン','スライム')
 excel_path = "漢字リスト.xlsx"  # 例: 同じディレクトリにある場合
 #excel_path = "data/漢字リスト.xlsx"  # 例: dataフォルダ内にある場合
 df = pd.read_excel(excel_path) 
@@ -29,8 +23,8 @@ def load_questions(excel_path):
     try:
         df = pd.read_excel(excel_path)  
         # '問題文' と '正解' という列名があることを前提
-        questions = df['問題文'].tolist()
-        answers = df['正解'].tolist()
+        questions = df['漢字'].tolist()
+        answers = df['読み'].tolist()
         return list(zip(questions, answers)) # 問題と解答をペアにしたリストを返す
     except FileNotFoundError:
         st.error(f"エラー: ファイル '{excel_path}' が見つかりません。")
