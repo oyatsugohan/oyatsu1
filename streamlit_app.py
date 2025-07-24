8120cdae43958ba669672e350ff855deb97be174
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-07-24 05:15:20 +0000

    気まぐれメルシィ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title('漢検練習帳')
 
-st.write('このアプリでは「漢検三級」「漢検二級」「漢検準一級」「漢検一級」の[漢字⇒よみ]　の問題が解けます！')
+st.write('このアプリでは「漢検三級」「漢検二級」「漢検準一級」「漢検一級」の')
+st.write('[漢字⇒よみ]　の問題が解けます！')
 player_name=st.text_input('あなたの名前を教えてください！')
 st.write(player_name+'さんですね！')
