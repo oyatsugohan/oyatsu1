2f3e3f9ea3bbc7c5eea2a6db7f579fdb8c7549ba
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-07-24 05:12:12 +0000

    神っぽいな

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title('漢検練習帳')
 
-st.write('あなたの名前を教えてください！')
-player_name=st.text_input('What is your name?')
+player_name=st.text_input('あなたの名前を教えてください！')
 st.write(player_name+'さんですね！')
