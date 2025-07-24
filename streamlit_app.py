768be7c8119b54de7e590251cfa53c68049cc567
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-07-24 05:18:41 +0000

    キラークイーン

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ st.title('漢検練習帳')
 
 st.write('このアプリでは「漢検三級」「漢検二級」「漢検準一級」「漢検一級」の[漢字⇒よみ]　の問題が解けます！')
 player_name=st.text_input('あなたの名前を教えてください！')
-st.write(player_name+'さんですね！')
+if player_name !=(''):
+    st.write(player_name+'さんですね！')
