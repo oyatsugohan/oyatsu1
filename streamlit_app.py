b6b5cf983d4b9b63ef66bbb59ac7e223632ca2af
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-07-24 05:14:30 +0000

    からくりピエロ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title('漢検練習帳')
 
+st.write('このアプリでは「漢検三級」「漢検二級」「漢検準一級」「漢検一級」の[漢字⇒よみ]　の問題が解けます！')
 player_name=st.text_input('あなたの名前を教えてください！')
 st.write(player_name+'さんですね！')
